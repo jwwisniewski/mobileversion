12ad71fe4f827b27fbf97a4da9d0992311ccd6d0
Jakub Wiśniewski <j.w.wisniewski@gmail.com>
2016-03-16 06:56:44 +0100

    JCM Model abstract class, category and subpage model

---
+++ app/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+class Category extends JCMModel {
+
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'devel_produkty_kategoria';
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id_kategoria';
+
+}
---
+++ app/Subpage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+class Subpage extends JCMModel {
+
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'devel_subpage';
+
+  /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id_subpage';
+
+}
