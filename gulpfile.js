7249d4b291aec8a9643c54190fb4fe193a20818d
Jakub Wiśniewski <j.w.wisniewski@gmail.com>
2016-04-03 19:57:28 +0200

    gulpfile - uncombine vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var paths = {
   'placeholders': '/bower_components/placeholders/dist'
 };
 
-elixir.config.sourcemaps = false;
-
 elixir(function (mix) {
   mix
           .scripts([
@@ -17,15 +15,12 @@ elixir(function (mix) {
           .scripts([
             'app.js'
           ], 'public/js/app.js')
-          .combine(['public/js/vendor.js', 'public/js/app.js'], 'public/js/combined.js')
           .styles([
             paths.bootstrap + '/css/bootstrap.css',
           ], 'public/css/vendor.css', './')
           .styles(['icomoon/style.css', 'theme.css'], 'public/css/theme.css')
           .less(['app.less'], 'public/css/app.css')
-          .combine(['public/css/vendor.css', 'public/css/theme.css', 'public/css/app.css'], 'public/css/combined.css')
-//          .version(['js/vendor.js', 'js/app.js', 'css/vendor.css','css/theme.css', 'css/app.css'])
-          .version(['js/combined.js', 'css/combined.css'])
+          .version(['js/vendor.js', 'js/app.js', 'css/vendor.css','css/theme.css', 'css/app.css'])
           .copy('resources/assets/css/icomoon/fonts', 'public/build/css/fonts')
           ;
 });
