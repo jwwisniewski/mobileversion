6f3b0fc303080ad6689412d6d9f78fa352e80da3
Jakub Wiśniewski <j.w.wisniewski@gmail.com>
2016-04-26 22:45:42 +0200

    logo

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,10 @@ elixir(function (mix) {
             paths.bootstrap + '/css/bootstrap.css'
           ], 'public/css/vendor.css', './')
           .styles(['icomoon/style.css'], 'public/css/fonts.css')
-          .less(['app.less', 'theme.less'], 'public/css/app.css')
+          .less(['theme.less', 'app.less'], 'public/css/app.css')
           .version(['js/vendor.js', 'js/app.js', 'css/vendor.css','css/fonts.css', 'css/app.css'])
+          .copy('resources/assets/img', 'public/build/img')
+//          .version(['img/ll600.png'])
           .copy('resources/assets/css/icomoon/fonts', 'public/build/css/fonts')
           ;
 });
